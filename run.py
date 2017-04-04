45b2fc23f0701aa6ebbdbec62e7dfeda010d0eb0
Cagin Gulsen <cagingulsen@gmail.com>
2017-04-04 12:42:12 +0200

    Exact name match added

--- run.py
+++ run.py
@@ -18,6 +18,8 @@ KNOWLEDGE_API_URL = 'https://kgsearch.googleapis.com/v1/entities:search'
 LMK_COMMAND = "lmk"
 
 
+print("PanpisBot starting...")
+
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id,
                     text="Type '/lmk <something>' to use this bot.")
@@ -25,6 +27,7 @@ def start(bot, update):
 
 def let_me_know(bot, update):
     query = update.message.text[len(LMK_COMMAND)+2:]
+    print("Received query: ",query)
     response = update.message.text
 
     params = {
@@ -41,11 +44,24 @@ def let_me_know(bot, update):
 
     final_score = 0
     final_item = None
+	
+    # search items for an exact name match
     for item in items:
-        score = item.get('resultScore', None)
-        if score is not None and (score > final_score):
-            final_score = score
-            final_item = item
+        result = item.get("result", None)
+        if result is not None:	
+            name = result.get("name", None)
+            if name is not None and name.lower() == query.lower():
+                final_score = score
+                final_item  = item
+                break
+
+    # if no exact name match is found, get the item with the highest score
+    if final_item is None:
+        for item in items:
+            score = item.get('resultScore', None)
+            if score is not None and (score > final_score):
+                final_score = score
+                final_item  = item
 
     if final_item is not None:
         result = final_item.get("result", None)
@@ -60,7 +76,6 @@ def let_me_know(bot, update):
 
     bot.sendMessage(chat_id=update.message.chat_id, text=response)
 
-
 updater = Updater(token=TELEGRAM_TOKEN)
 dispatcher = updater.dispatcher
 
@@ -69,5 +84,7 @@ lmk_handler = CommandHandler(LMK_COMMAND, let_me_know)
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(lmk_handler)
+
+print("PanpisBot ready to go!")
 
 updater.start_polling()
