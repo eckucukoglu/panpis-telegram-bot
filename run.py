6cfca305e93239c2fe4f4f9fb8c2a00e299327b8
Emre Can Kucukoglu <eckucukoglu@gmail.com>
2017-06-19 11:46:58 +0300

    some bugs resolved.

--- run.py
+++ run.py
@@ -1,24 +1,23 @@
 # -*- coding: utf-8 -*-
 
 import codecs
-import logging
 import json
 import urllib
 import urllib.parse
 import urllib.request
 
 from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
 from telegram.ext import Updater
 
 TELEGRAM_TOKEN = open('.telegram_token').readline()[:-1]
 KNOWLEDGE_API_TOKEN = open('.gcloud_api_token').readline()[:-1]
 KNOWLEDGE_API_URL = 'https://kgsearch.googleapis.com/v1/entities:search'
 
+START_COMMAND = "start"
 LMK_COMMAND = "lmk"
 
-
 print("PanpisBot starting...")
+
 
 def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id,
@@ -27,8 +26,6 @@ def start(bot, update):
 
 def let_me_know(bot, update):
     query = update.message.text[len(LMK_COMMAND)+2:]
-    print("Received query: ",query)
-    response = update.message.text
 
     params = {
         'query': query,
@@ -41,18 +38,19 @@ def let_me_know(bot, update):
     reader = codecs.getreader("utf-8")
     obj = json.load(reader(urllib.request.urlopen(url)))
     items = obj.get("itemListElement", None)
-
+    # print(obj)
     final_score = 0
     final_item = None
-	
+
     # search items for an exact name match
     for item in items:
         result = item.get("result", None)
-        if result is not None:	
+        if result is not None:
             name = result.get("name", None)
+            score = item.get('resultScore', None)
             if name is not None and name.lower() == query.lower():
                 final_score = score
-                final_item  = item
+                final_item = item
                 break
 
     # if no exact name match is found, get the item with the highest score
@@ -61,30 +59,41 @@ def let_me_know(bot, update):
             score = item.get('resultScore', None)
             if score is not None and (score > final_score):
                 final_score = score
-                final_item  = item
+                final_item = item
 
+    response = "not found " + query
     if final_item is not None:
         result = final_item.get("result", None)
-        if result is None:
-            response = "None result"
-        else:
-            description = result.get("detailedDescription", None)
-            if description is None:
-                response = "None description"
+        if result is not None:
+            detailedDescription = result.get("detailedDescription", None)
+            if detailedDescription is None:
+                # try description
+                description = result.get("description", None)
+                if description is not None:
+                    response = description
             else:
-                response = description.get("articleBody", None)
+                response = detailedDescription.get("articleBody", None)
 
+    print(query, " : ", response)
     bot.sendMessage(chat_id=update.message.chat_id, text=response)
 
-updater = Updater(token=TELEGRAM_TOKEN)
-dispatcher = updater.dispatcher
+def lambda_handler(event, context):
+    updater = Updater(token=TELEGRAM_TOKEN)
+    dispatcher = updater.dispatcher
 
-start_handler = CommandHandler('start', start)
-lmk_handler = CommandHandler(LMK_COMMAND, let_me_know)
+    start_handler = CommandHandler(START_COMMAND, start)
+    lmk_handler = CommandHandler(LMK_COMMAND, let_me_know)
 
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(lmk_handler)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(lmk_handler)
 
-print("PanpisBot ready to go!")
+    print("PanpisBot ready to go!")
 
-updater.start_polling()
+    updater.start_polling()
+
+
+if __name__ == '__main__':
+    event = {
+
+    }
+    lambda_handler(event, None)
